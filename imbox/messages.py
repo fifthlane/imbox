16fce82cfa61c887201aa12f7de48bc877b2226a
Justin Copeland <copeland.justin@gmail.com>
2022-06-10 11:44:24 -0700

    add ability to reverse messages to parse oldest first

--- imbox/messages.py
+++ imbox/messages.py
@@ -29,14 +29,16 @@ class Messages:
     def __init__(self,
                  connection,
                  parser_policy,
+                 reversed,
                  **kwargs):
 
         self.connection = connection
         self.parser_policy = parser_policy
+        self.reversed = reversed
         self.kwargs = kwargs
-        self._uid_list = self._query_uids(**kwargs)
-
-        logger.debug("Fetch all messages for UID in {}".format(self._uid_list))
+        self._uid_list = reversed(self._query_uids(**kwargs)) if self.reversed else self._query_uids(**kwargs)
+        
+        logger.debug(f"Fetch all messages for UID in {self._uid_list}")
 
     def _fetch_email(self, uid):
         return fetch_email_by_uid(uid=uid,
